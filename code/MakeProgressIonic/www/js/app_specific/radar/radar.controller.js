3994f296e5fe1b9ade284a3ccc253aa39bafa29c
Binary Decoder <binarydecoder01@gmail.com>
2019-03-18 12:26:00 +0000

    a lot of changes for radar chart
    The radar module and controller is in app.js
    We still need to migrate that to radar folder under module and controller

---
+++ code/MakeProgressIonic/www/js/app_specific/radar/radar.controller.js
@@ -0,0 +1,27 @@
+/* (function () {
+    'use strict';
+
+    angular
+        .module('radarjs')
+        .controller('RadarCtrl', control);
+
+    /control.$inject = [
+        '$state',
+        'templatesSrvc'
+        ];
+    
+    function control(
+    ) {
+        var vm = angular.extend(this, {
+            labels : ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+          
+            data : [
+              [65, 59, 90, 81, 56, 55, 40],
+              [28, 48, 40, 19, 96, 27, 100]
+            ]
+         });
+
+        
+    }
+})();
+ */
