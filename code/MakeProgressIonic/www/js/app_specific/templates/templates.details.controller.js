fd038760a8466174fab0080428c26faaf89bd1c6
robflynnyh <44401820+robflynnyh@users.noreply.github.com>
2019-03-19 16:11:29 +0000

    Delete button

--- code/MakeProgressIonic/www/js/app_specific/templates/templates.details.controller.js
+++ code/MakeProgressIonic/www/js/app_specific/templates/templates.details.controller.js
@@ -29,6 +29,16 @@
         vm.goBack = function() {
             $state.go('templates_list');
         }
+		vm.deleteThis = function(){
+			var temparray = [];
+			for(var i=0;i<JSON.parse(localStorage.getItem('storeTemplate')).length;i++){
+				if(JSON.parse(localStorage.getItem('storeTemplate'))[i].name != templatesSrvc.getTemplateAt(params.selected3).name && JSON.parse(localStorage.getItem('storeTemplate'))[i].attributes != templatesSrvc.getTemplateAt(params.selected3).attributes){
+					temparray.push(JSON.parse(localStorage.getItem('storeTemplate'))[i]);
+				}
+			}
+			localStorage.setItem("storeTemplate", JSON.stringify(temparray));
+			$state.go('templates_list');
+		}
 
         var params = $stateParams;
         vm.template = templatesSrvc.getTemplateAt(params.selected3);
